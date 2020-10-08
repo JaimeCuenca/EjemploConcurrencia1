24575123f018c51a3c6dc2a26ee410ca23622dc2
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2020-10-08 12:17:26 +0200

    añadidos try-catch

--- src/CustomThread.java
+++ src/CustomThread.java
@@ -1,8 +1,13 @@
-public class CustomThread extends Thread{
+public class CustomThread extends Thread {
     @Override
     public void run() {
-        for(int i = 0; i < 10; i++){
-            System.out.println("Repetición número. " + i);
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Repetición número. " + i);
+                sleep(500); //simula una pausa en el sistema de X milisegundos
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,27 @@ public class Main {
         System.out.println("EL tiempo transcurrido es: "+ (System.currentTimeMillis()-tInicio) +" milisegundos");
 
         System.out.println("Terminado"); //Se ejecuta antes porque está en primer plano
+
+        CustomThread t1 = new CustomThread();
+        t1.start();
+        try {
+            t1.join(); //Espera a que termine el Thread
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+        CustomThread t2 = new CustomThread();
+        t2.start();
+        try {
+            t2.join(); //Espera a que termine el Thread
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+        CustomThread t3 = new CustomThread();
+        t3.start();
+        try {
+            t3.join(); //Espera a que termine el Thread
+        }catch(Exception e){
+            System.out.println("Error");
+        }
     }
 }
