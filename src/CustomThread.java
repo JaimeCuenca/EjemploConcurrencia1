539271b0bd961f91fc45cbf5d91a5973b43579a1
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2020-10-08 12:31:46 +0200

    añadido constructor y atributo de espera

--- src/CustomThread.java
+++ src/CustomThread.java
@@ -1,10 +1,17 @@
 public class CustomThread extends Thread {
-    @Override
+    private int tiempodeEspera;
+
+    public CustomThread(int tiempodeEspera) {
+        this.tiempodeEspera = tiempodeEspera;
+    }
+
+        @Override
     public void run() {
+
         try {
             for (int i = 0; i < 10; i++) {
                 System.out.println("Repetición número. " + i);
-                sleep(500); //simula una pausa en el sistema de X milisegundos
+                sleep(tiempodeEspera); //simula una pausa en el sistema de X milisegundos
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         long tInicio = System.currentTimeMillis();
-        CustomThread t = new CustomThread();
+        CustomThread t = new CustomThread(1);
         t.start(); //Se manda a hacer en segundo plano
 
         try {
@@ -14,23 +14,15 @@ public class Main {
 
         System.out.println("Terminado"); //Se ejecuta antes porque está en primer plano
 
-        CustomThread t1 = new CustomThread();
-        t1.start();
         try {
+            CustomThread t1 = new CustomThread(1000);
+            t1.start();
             t1.join(); //Espera a que termine el Thread
-        }catch(Exception e){
-            System.out.println("Error");
-        }
-        CustomThread t2 = new CustomThread();
-        t2.start();
-        try {
+            CustomThread t2 = new CustomThread(2000);
+            t2.start();
             t2.join(); //Espera a que termine el Thread
-        }catch(Exception e){
-            System.out.println("Error");
-        }
-        CustomThread t3 = new CustomThread();
-        t3.start();
-        try {
+            CustomThread t3 = new CustomThread(3000);
+            t3.start();
             t3.join(); //Espera a que termine el Thread
         }catch(Exception e){
             System.out.println("Error");
